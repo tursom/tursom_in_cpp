cf5cf826d1fc9cb773fd5e31ffeb8e09f71ba8e7
tursom <tursom@foxmail.com>
2021-05-22 22:26:19 +0800

    create project

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "proto/TursomMsg.pb.h"
+
+int main() {
+    cn::tursom::im::protobuf::ImMsg msg;
+    msg.set_msgid("test msg id");
+    //*msg.mutable_msgid() = "test msg id";
+
+    msg.mutable_chatmsg()->set_sender("test sender");
+    std::cout << msg.DebugString() << std::endl;
+    return 0;
+}
